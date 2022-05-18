224ef82d2ab8e1fbcb1f60245b1e283ffffe59d9
Damian Borowiecki <dborowiecki@soldevelo.com>
2022-05-18 10:03:02 +0200

    OP-777: Fixed absolute mock in unit test

--- tools/tests/test_services.py
+++ tools/tests/test_services.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, PropertyMock
 
 from django.test import TestCase
 from unittest import mock
@@ -8,27 +8,27 @@ from xml.etree import ElementTree
 
 
 class UploadClaimsTestCase(TestCase):
-    @mock.patch('tools.services.django.conf')
-    def test_upload_claims_unknown_hf(self, row_security_mock):
-        row_security_mock.settings.ROW_SECURITY = MagicMock(return_value=True)
-        mock_user = mock.Mock(is_anonymous=False)
-        mock_user.has_perm = mock.MagicMock(return_value=True)
-        with self.assertRaises(InvalidXMLError) as cm:
-            upload_claim(
-                mock_user,
-                ElementTree.fromstring(
-                    """
-                        <root>
-                            <Claim>
-                                <Details>
-                                    <HFCode>WRONG</HFCode>
-                                </Details>
-                            </Claim>
-                        </root>
-                    """
-                ),
+    def test_upload_claims_unknown_hf(self):
+        with patch('tools.services.settings.ROW_SECURITY', new_callable=PropertyMock) as row_security_mock:
+            row_security_mock.return_value = True
+            mock_user = mock.Mock(is_anonymous=False)
+            mock_user.has_perm = mock.MagicMock(return_value=True)
+            with self.assertRaises(InvalidXMLError) as cm:
+                upload_claim(
+                    mock_user,
+                    ElementTree.fromstring(
+                        """
+                            <root>
+                                <Claim>
+                                    <Details>
+                                        <HFCode>WRONG</HFCode>
+                                    </Details>
+                                </Claim>
+                            </root>
+                        """
+                    ),
+                )
+            self.assertEqual(
+                "User cannot upload claims for health facility WRONG",
+                str(cm.exception),
             )
-        self.assertEqual(
-            "User cannot upload claims for health facility WRONG",
-            str(cm.exception),
-        )
