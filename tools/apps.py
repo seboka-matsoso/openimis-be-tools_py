9e71408bf51a861a97017dece1b63656b69aa3f6
Seboka Matsoso <69431946+seboka-matsoso@users.noreply.github.com>
2024-01-29 16:50:30 +0200

    added tools to github

--- tools/apps.py
+++ tools/apps.py
@@ -26,7 +26,6 @@ class ToolsConfig(AppConfig):
     registers_locations_perms = []
     registers_items_perms = []
     registers_services_perms = []
-
     extracts_master_data_perms = []
     extracts_officer_feedbacks_perms = []
     extracts_officer_renewals_perms = []
@@ -38,26 +37,18 @@ class ToolsConfig(AppConfig):
     def _configure_permissions(self, cfg):
         ToolsConfig.registers_perms = cfg["registers_perms"]
         ToolsConfig.registers_diagnoses_perms = cfg["registers_diagnoses_perms"]
-        ToolsConfig.registers_health_facilities_perms = cfg[
-            "registers_health_facilities_perms"
-        ]
+        ToolsConfig.registers_health_facilities_perms = cfg["registers_health_facilities_perms"]
         ToolsConfig.registers_items_perms = cfg["registers_items_perms"]
         ToolsConfig.registers_services_perms = cfg["registers_services_perms"]
         ToolsConfig.registers_locations_perms = cfg["registers_locations_perms"]
         ToolsConfig.extracts_master_data_perms = cfg["extracts_master_data_perms"]
         ToolsConfig.extracts_phone_extract_perms = cfg["extracts_phone_extract_perms"]
         ToolsConfig.extracts_upload_claims_perms = cfg["extracts_upload_claims_perms"]
-
-        ToolsConfig.extracts_officer_feedbacks_perms = cfg[
-            "extracts_officer_feedbacks_perms"
-        ]
-        ToolsConfig.extracts_officer_renewals_perms = cfg[
-            "extracts_officer_renewals_perms"
-        ]
+        ToolsConfig.extracts_officer_feedbacks_perms = cfg["extracts_officer_feedbacks_perms"]
+        ToolsConfig.extracts_officer_renewals_perms = cfg["extracts_officer_renewals_perms"]
 
     def ready(self):
         from core.models import ModuleConfiguration
-
         cfg = ModuleConfiguration.get_or_default("tools", DEFAULT_CFG)
         for key, value in cfg.items():
             print(f"{key}: {value}")
